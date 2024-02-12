b85bd09858bd40028f429ebc44f00e22750dda49
kirtandave03 <kirtan.dave@bosctechlabs.com>
2024-02-12 18:26:53 +0530

    Kirtan

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -24,83 +24,73 @@ const userSchema = new mongoose.Schema({
         requirerd: true,
         unique: true
     },
-    address:{
-        type :String
-    }
-    //  [
-    //     {
-    //         house: {
-    //             type: String,
-    //             required: true
-    //         },
-    //         area: {
-    //             type: String,
-    //             required: true
-    //         },
-    //         landmark: {
-    //             type: String
-    //         },
-    //         pincode: {
-    //             type: Number,
-    //             required: true
-    //         },
-    //         town: {
-    //             type: String,
-    //             required: true
-    //         },
-    //         state: {
-    //             type: String,
-    //             required: true
-    //         },
-    //         country : {
-    //             type :String,
-    //             required : true
-    //         },
-    //         adType: {
-    //             type: String,
-    //             enum: ["Home", "Office", "Other"]
-    //         }
-    //     }
-    // ],
-    // member: {
-    //     type: Boolean,
-    //     required: true,
-    //     default: false
-    // },
-    // status: {
-    //     type: String,
-    //     enum: ["active", "inactive"],
-    //     default: "active"
-    // },
-    // wishlist: [
-    //     {
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: "Product"
-    //     }
-    // ],
-    // cart: [
-    //     {
-    //         product: {
-    //             type: mongoose.Schema.Types.ObjectId,
-    //             ref: "Product"
-    //         },
-    //         productImage: {
-    //             type: String
-    //         },
-    //         quantity: {
-    //             type: Number,
-    //             required: true
-    //         },
-    //         price : {
-    //             type : String,
-    //             required : true
-    //         },
-    //         totalAmount : {
-    //             type: String,
-    //             required : true
-    //         },
-    //     }
-    // ]
+    address:  [
+        {
+            house: {
+                type: String,
+                required: true
+            },
+            area: {
+                type: String,
+                required: true
+            },
+            pincode: {
+                type: Number,
+                required: true
+            },
+            town: {
+                type: String,
+                required: true
+            },
+            state: {
+                type: String,
+                required: true
+            },
+            adType: {
+                type: String,
+                enum: ["Home", "Office", "Other"]
+            }
+        }
+    ],
+    member: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active"
+    },
+    wishlist: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product"
+        }
+    ],
+    cart: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product"
+            },
+            productImage: {
+                type: String
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            price : {
+                type : String,
+                required : true
+            },
+            totalAmount : {
+                type: String,
+                required : true
+            },
+        }
+    ]
 }, {timestamps: true})
 
 userSchema.methods.genarateAccessToken = function(){
