2634f0e8e7aa3f2c7059c3f6968372bfcd4da92c
airaawat <miraj.doshi@bosctechlabs.com>
2024-03-20 10:53:57 +0530

    changed secure to false

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ const asyncHandler = require("../utils/asyncHandler");
 
 const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
-    const token = req.header("auth-token");
+    const token = req.cookie.accessToken;
 
     if (!token) {
       // check the status code use 401
