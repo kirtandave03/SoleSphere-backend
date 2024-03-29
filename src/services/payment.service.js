d650e0df1802312f2a0db12a69a4d702836932fd
Kirtan Dave <kirtan.dave@boscyechlabs.com>
2024-03-29 19:29:52 +0530

    razor pay

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -110,7 +110,7 @@ class PaymentService {
     //     throw new apiError(400, "Invalid signature sent!");
     //   }
 
-    console.log("Req Body by RazorPay", req.body);
+    console.log("Req Body by RazorPay", req.body.payload.payment.entity);
     const shasum = crypto.createHmac(
       "sha256",
       process.env.RAZORPAY_WEBHOOK_SECRET
