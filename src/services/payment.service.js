25a1bd590ef57e38fdf2fab3e2c4a7e406fad25e
Kirtan Dave <kirtan.dave@boscyechlabs.com>
2024-03-29 17:01:28 +0530

    fixed bug in razorpay payment verification

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -110,6 +110,8 @@ class PaymentService {
     //     throw new apiError(400, "Invalid signature sent!");
     //   }
 
+    console.log(req.body);
+
     res.status(200).json({ success: true });
   };
 
